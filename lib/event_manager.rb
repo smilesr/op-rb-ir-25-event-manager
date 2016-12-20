fde5938a9065bc8f4fe3f55050c82bff439aaa6c
Robert Smiles Jr <smilesr120@gmail.com>
2016-12-20 15:47:13 -0500

    replace api gem with own code

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,15 +1,21 @@
+require 'sinatra'
 require 'csv'
-require 'sunlight/congress'
 require 'erb'
+require 'sinatra/config_file'
+require 'open-uri'
+require 'JSON'
+require 'pry'
 
-Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+config_file '../config.yml'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
 def legislators_by_zipcode(zipcode)
-  Sunlight::Congress::Legislator.by_zipcode(zipcode)
+  response = open("https://congress.api.sunlightfoundation.com/legislators/locate?zip=#{zipcode}", "x-api-key" => settings.sunshine_api_key)
+  parsed_response=JSON.parse(response.read)
+  array_of_legislators = parsed_response["results"]
 end
 
 def save_thank_you_letters(id,form_letter)
@@ -24,9 +30,9 @@ end
 
 puts "EventManager initialized."
 
-contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+contents = CSV.open '../event_attendees.csv', headers: true, header_converters: :symbol
 
-template_letter = File.read "form_letter.erb"
+template_letter = File.read "views/form_letter.erb"
 erb_template = ERB.new template_letter
 
 contents.each do |row|
@@ -34,6 +40,7 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
+  binding.pry
 
   form_letter = erb_template.result(binding)
 
